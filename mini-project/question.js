3212c8f2a7633a6dd2937529fb159799400ce515
kehinde cwb <85681159+codewithbillions@users.noreply.github.com>
2023-05-23 17:45:43 +0100

    quiz app

---
+++ mini-project/question.js
@@ -0,0 +1,67 @@
+export default [
+    {
+        question: 'what is the gomycode color ?',
+        answers: [
+            {option: 'red white  black', correct: true},
+            {option: 'red yellow green', correct: false},
+            {option: 'green white green', correct: false},
+        ],
+    },
+    {
+        question: 'what basketball team does Lebron James play for?',
+        answers: [
+            {option: 'miami heats', correct: false},
+            {option: 'chicago bulls', correct: false},
+            {option: 'LA lakers', correct: true},
+        ],
+    },
+    {
+        question: 'what year was youtube created?',
+        answers: [
+            {option: '2007', correct: false},
+            {option: '2005', correct: true},
+            {option: '2000', correct: false},
+        ],
+    },
+    {
+        question: 'what is Gomycode Headquater located?',
+        answers: [
+            {option: 'Abuja', correct: false},
+            {option: 'Kano', correct: false},
+            {option: 'Lagos', correct: true},
+        ],
+    },
+    {
+        question: 'what is BZ best food?',
+        answers: [
+            {option: 'amala and ewedu', correct: false},
+            {option: 'beans and yam', correct: false},
+            {option: 'Ogbono soup', correct: true},
+        ],
+    },
+    {
+        question: 'what is the chemical symbol of water?',
+        answers: [
+            {option: 'NaCl', correct: false},
+            {option: 'H2O', correct: true},
+            {option: 'H2SO4', correct: false},
+        ],
+    },
+    {
+        question: 'who is the number one wresler in WWE?',
+        answers: [
+            {option: 'The miz', correct: false},
+            {option: 'Rock', correct: true},
+            {option: 'USOS', correct: false},
+        ],
+    },
+    {
+        question: 'who founded YPMC?',
+        answers: [
+            {option: 'Daniel', correct: false},
+            {option: 'Agbado', correct: false},
+            {option: 'Malcolm', correct: true},
+        ],
+
+    },
+]
